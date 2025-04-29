29ea311c365471e27242c0a9af92963afb6cbfa8
Suraj Yadav <itssurajyadav7@gmail.com>
2025-04-29 18:32:54 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os  
 import sys      
                
-def main():            
+def main():                
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlineco.settings')               
     try:                        
         from django.core.management import execute_from_command_line                         
